0baa88503d74b0e885319c0537f95ee40f6f3f4c
jianzhang <jianzhang0412@gmail.com>
2021-09-09 17:24:00 -0400

    Add styled-components and some placeholders

--- client/relatedComparison/Card.jsx
+++ client/relatedComparison/Card.jsx
@@ -1,12 +1,23 @@
 import React from 'react';
-
+import styled from 'styled-components';
 
 var Card =() =>{
   return (
-    <div>
-      <h1>my list</h1>
-    </div>
+  <CardStyle>
+    <img></img>
+      <div class="container">
+        <p>Picture</p>
+        <h4>Title</h4>
+        <p>Description</p>
+      </div>
+  </CardStyle>
     )
-}
+};
 
+var CardStyle = styled.div`
+  background: lightgreen;
+  width:150px;
+  height:200px;
+  margin-left:10px;
+`;
 export default Card;
