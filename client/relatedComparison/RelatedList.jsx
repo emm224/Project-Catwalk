977c6bf0408d7be84432b239dc2f6758591a14b4
jianzhang <jianzhang0412@gmail.com>
2021-09-09 16:37:46 -0400

    Crate Card and relatedlist components

---
+++ client/relatedComparison/RelatedList.jsx
@@ -0,0 +1,15 @@
+import React from 'react';
+
+
+
+class RelatedList extends React.Component {
+  render () {
+    return (
+    <div>
+      <h1>my list</h1>
+    </div>
+    )
+  }
+}
+
+export default RelatedList;
