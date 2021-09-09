65f0d211b928d3590b963a66078185072f360504
Isaac Favila <imfavila@gmail.com>
2021-09-09 18:18:49 -0500

    intialize server

---
+++ server/index.js
@@ -0,0 +1,16 @@
+const express = require('express');
+const morgan = require('morgan');
+
+let app = express();
+let port = process.env.PORT || 3000;
+
+
+app.use(express.static(__dirname + '/../public'));
+app.use(express.json());
+app.use(express.urlcoded({extended: true}));
+app.use(morgan('dev'));
+
+
+app.listen(port, function() {
+  console.log(`listening on port ${port}`);
+});
