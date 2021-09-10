3ce231acd8bc7ff62c32214a30a8f3ef840869b5
Isaac Favila <85847561+IsaacFavila@users.noreply.github.com>
2021-09-10 12:38:05 -0500

    Update index.js

--- server/index.js
+++ server/index.js
@@ -7,7 +7,7 @@ let port = process.env.PORT || 3000;
 
 app.use(express.static(__dirname + '/../public'));
 app.use(express.json());
-app.use(express.urlcoded({extended: true}));
+app.use(express.urlencoded({extended: true}));
 app.use(morgan('dev'));
 
 
