4b542563118361f01e7e193fd0935fad8d3c75d8
Eric Ma <eric.ma95@gmail.com>
2021-09-10 13:36:56 -0500

    created ProudctOverview class

---
+++ client/Overview/index.jsx
@@ -0,0 +1,29 @@
+import React from 'react';
+import axios from 'axios';
+
+class Product extends React.Component {
+  constructor (props) {
+    super (props);
+    this.state = {
+      products
+    }
+  }
+
+  render() {
+    return (
+      <div>
+        <ImageGallery />
+        <StarRating />
+        <ProductDetail />
+        <PriceDisplay />
+        <StyleSelector />
+        <AddToCart />
+        <ProductDescription />
+        <ProductFeatures />
+      </div>
+    )
+  }
+
+}
+
+export default Product
