19558db4bcb1c29d3218297558340b0c2677b9e9
Isaac Favila <imfavila@gmail.com>
2021-09-14 18:05:00 -0500

    changed review entry to class and made a function to properly format the date

--- client/ratings-reviews/reviews/ReviewListEntry.jsx
+++ client/ratings-reviews/reviews/ReviewListEntry.jsx
@@ -1,25 +1,65 @@
 import React from 'react';
 import styled from 'styled-components';
 
-const ReviewListEntry = (props) => (
-  <ReviewListEntryStyle>
+class ReviewListEntry extends React.Component {
+  constructor(props) {
+    super(props);
+    this.state = {
+      date: ''
+    }
+  }
 
-    <TopRowStyle>
-      <div>★★★☆☆</div>
-      <div>
-        {props.review.reviewer_name}, {props.review.date.toString().slice(0,9)}
-      </div>
-    </TopRowStyle>
+  componentDidMount() {
+    this.formatDate();
+  }
 
-    <SummaryStyle> <b>{props.review.summary}</b> </SummaryStyle>
+  formatDate() {
+    // Get rid of unnecessary time zone and put into an array formatted MM/DD/YYYY
+    let date = this.props.review.date.toString().slice(0,9).split('-').reverse();
+    // Set numberical month to lexical month
+    var months = ['0', 'January', 'February', 'March', 'April', 'May', 'June', 'July', 'August', 'September', 'October', 'November', 'December'];
+    var i = Number(date[0]);
+    date[0] = months[i] + ' ';
+    // Get rid of extra 0 if applicable
+    if (date[1][0] === '0') {
+      date[1] = date[1][1];
+    }
+    date[1] = date[1] + ', ';
+    // Join together and add to state
+    var formatted = date.join('');
+    this.setState({
+      date: formatted
+    })
+  }
 
-    <SummaryStyle>{props.review.body}</SummaryStyle>
+  render () {
+    return (
+      <ReviewListEntryStyle>
+
+        <TopRowStyle>
+          <div>★★★☆☆</div>
+          <div>
+            {this.props.review.reviewer_name}, {this.state.date}
+          </div>
+        </TopRowStyle>
+
+        <SummaryStyle> <b>{this.props.review.summary}</b> </SummaryStyle>
+
+        <SummaryStyle>{this.props.review.body}</SummaryStyle>
+
+        <ResponseStyle>
+          <b>Response: </b>
+          <div>{this.props.review.response}</div>
+        </ResponseStyle>
 
 
-    <BottomRowStyle>Helpful? <u>Yes</u> ({props.review.helpfulness}) | <u>Report</u></BottomRowStyle>
+        <BottomRowStyle>Helpful? <u>Yes</u> ({this.props.review.helpfulness}) | <u>Report</u></BottomRowStyle>
 
-  </ReviewListEntryStyle>
-);
+      </ReviewListEntryStyle>
+    );
+  }
+
+};
 
 ///////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////////
 
@@ -42,6 +82,13 @@ var SummaryStyle = styled.div`
 var BottomRowStyle = styled.div`
   margin-bottom: 10px;
   font-size: 12px;
+`;
+
+var ResponseStyle = styled.div`
+  margin-bottom: 10px;
+  background-color: lightgray;
+  padding: 10px;
+  margin-bottom: 10px;
 `;
 
 export default ReviewListEntry;
