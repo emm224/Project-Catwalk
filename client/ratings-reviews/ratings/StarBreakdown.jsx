85d01b38eaaffbe7634138a4ce36b5bc915df8e9
Isaac Favila <imfavila@gmail.com>
2021-09-14 18:43:08 -0500

    more static formatting of the ratings component

--- client/ratings-reviews/ratings/StarBreakdown.jsx
+++ client/ratings-reviews/ratings/StarBreakdown.jsx
@@ -10,15 +10,18 @@ class StarBreakdown extends React.Component {
     return (
       <div>
         <StarBreakdownStyle>
-          <NumberStyle>3</NumberStyle>
+          <NumberStyle>3.5</NumberStyle>
           <div>★★★☆☆</div>
         </StarBreakdownStyle>
-        <div>100% of reviews recommendt this product</div>
-        <div><u>5 star(s)</u></div>
-        <div><u>4 star(s)</u></div>
-        <div><u>3 star(s)</u></div>
-        <div><u>2 star(s)</u></div>
-        <div><u>1 star(s)</u></div>
+
+        <StarBarGraphStyle>100% of reviews recommend this product</StarBarGraphStyle>
+
+        <StarBarGraphStyle><u>5 star(s) </u> =========================</StarBarGraphStyle>
+        <StarBarGraphStyle><u>4 star(s)</u> =========================</StarBarGraphStyle>
+        <StarBarGraphStyle><u>3 star(s)</u> =========================</StarBarGraphStyle>
+        <StarBarGraphStyle><u>2 star(s)</u> =========================</StarBarGraphStyle>
+        <StarBarGraphStyle><u>1 star(s) </u> =========================</StarBarGraphStyle>
+
       </div>
     );
   }
@@ -26,12 +29,17 @@ class StarBreakdown extends React.Component {
 
 var StarBreakdownStyle = styled.div`
   display: flex;
-
   margin-bottom: 20px;
+`;
+
+var StarBarGraphStyle = styled.div`
+  margin-bottom: 15px;
+  font-size: 12px;
 `;
 
 var NumberStyle = styled.div`
   font-size: 48px;
+  margin-right: 10px;
 `;
 
 
